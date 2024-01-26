b1ff6500765fb8280bf2381b05892d0c9a35ef42
Ben Drews <benjamindrews@gmail.com>
2024-01-25 22:26:31 -0500

    Fixed bug with plated armor getting ticked down by Fatigue type damage

--- ScheduleMod/src/main/java/schedulemod/patches/FatigueDamageTypePatch.java
+++ ScheduleMod/src/main/java/schedulemod/patches/FatigueDamageTypePatch.java
@@ -8,6 +8,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInsertLocator;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
@@ -15,6 +16,9 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.PlatedArmorPower;
+
 import javassist.CannotCompileException;
 import javassist.CtBehavior;
 import schedulemod.powers.FatiguePower;
@@ -37,7 +41,7 @@ public class FatigueDamageTypePatch {
 
 
 
-     @SpirePatch(clz = AbstractMonster.class, method = "damage")
+    @SpirePatch(clz = AbstractMonster.class, method = "damage")
     public static class FatigueDTDamagePatch {
 
       private static class Locator extends SpireInsertLocator {
@@ -52,6 +56,18 @@ public class FatigueDamageTypePatch {
       public static SpireReturn<Void> Insert(AbstractMonster obj, DamageInfo info, int damageAmount) {
         if (info.type == FatigueDamageTypePatch.FATIGUE) {
           AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(obj, info.owner, new FatiguePower(obj, info.owner, damageAmount)));
+          return SpireReturn.Return();
+        }
+        return SpireReturn.Continue();
+      }
+    }
+
+    @SpirePatch(clz = PlatedArmorPower.class, method = "wasHPLost")
+    public static class FatigueDTWasHPLostPatch {
+
+      @SpirePrefixPatch
+      public static SpireReturn<Void> Prefix(PlatedArmorPower obj, DamageInfo info, int damageAmount) {
+        if (info.type == FatigueDamageTypePatch.FATIGUE) {
           return SpireReturn.Return();
         }
         return SpireReturn.Continue();
