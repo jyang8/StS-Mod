4f431ee49b51a4a11b50a2217cba42478a65ee9c
Ben Drews <benjamindrews@gmail.com>
2024-02-11 18:09:24 -0500

    Added Rain Check

---
+++ ScheduleMod/src/main/java/schedulemod/actions/RainCheckAction.java
@@ -0,0 +1,57 @@
+package schedulemod.actions;
+
+import java.util.ArrayList;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.orbs.EmptyOrbSlot;
+
+import schedulemod.character.Entropy;
+import schedulemod.orbs.ScheduleOrb;
+
+public class RainCheckAction extends AbstractGameAction {
+
+    private static final float STARTING_DURATION = 0.2F;
+
+    public RainCheckAction() {
+        duration = STARTING_DURATION;
+    }
+
+    @Override
+    public void update() {
+        if (duration == STARTING_DURATION) {
+            AbstractPlayer p = AbstractDungeon.player;
+            if (!(p instanceof Entropy)) {
+                tickDuration();
+                isDone = true;
+                return;
+            }
+            ArrayList<ScheduleOrb> schedOrbs = new ArrayList<ScheduleOrb>();
+            for (AbstractOrb orb : p.orbs) {
+                if (orb instanceof ScheduleOrb) {
+                    schedOrbs.add((ScheduleOrb) orb);
+                }
+            }
+            if (schedOrbs.size() == 0) {
+                tickDuration();
+                isDone = true;
+                return;
+            }
+            int slot = p.orbs.size() - 1;
+            for (int i = schedOrbs.size() - 1; i >= 0; i--) {
+                p.orbs.set(slot, schedOrbs.get(i));
+                schedOrbs.get(i).setSlot(slot, p.orbs.size());
+            }
+            while (slot >= 0) {
+                AbstractOrb empty = new EmptyOrbSlot();
+                p.orbs.set(slot, empty);
+                empty.setSlot(slot, p.orbs.size());
+                slot--;
+            }
+            tickDuration();
+            isDone = true;
+        }
+    }
+}
---
+++ ScheduleMod/src/main/java/schedulemod/cards/navy/RainCheck.java
@@ -0,0 +1,39 @@
+package schedulemod.cards.navy;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import schedulemod.actions.RainCheckAction;
+import schedulemod.character.Entropy;
+import schedulemod.util.CardStats;
+
+public class RainCheck extends BaseCard {
+    public static final String ID = makeID(RainCheck.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Entropy.Enums.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            1);
+
+    private static final int BLOCK = 9;
+    private static final int UPGRADE_BLOCK = 4;
+
+    public RainCheck() {
+        super(ID, info);
+        setBlock(BLOCK, UPGRADE_BLOCK);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, this.block));
+        addToBot(new RainCheckAction());
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new RainCheck();
+    }
+}
